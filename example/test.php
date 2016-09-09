7dc0c46bcb401402ef40192ee39267510cce3009
Hashman Lin <coosos810609@gmail.com>
2016-09-09 14:22:47 +0800

    add log mode

--- example/test.php
+++ example/test.php
@@ -4,7 +4,10 @@
 
     use Track\Track;
 
+    // test for unlog mode
     $track = new Track();
+    // test for log mode
+    //$track = new Track('Hash Test Program', true);
     for ($i = 0; $i<5; $i++) {
         for ($j = 0; $j<10000000; $j++) {}
         $track->addCheckPoint("Run {$i} Time");
--- src/Track.php
+++ src/Track.php
@@ -25,18 +25,31 @@
         private $_total_second;
         private $_current_time;
         private $_msg;
+        private $_log_mode;
+        private $_log_path;
+        private $_log_file_name;
+        private $_f;
         /*
          * $data = [[name => 'DB query', 'time' => '11:29:10']]
          */
         private $_data = [];
 
-        public function __construct($msg = 'Program start')
+        public function __construct($msg = 'Program start', $log_mode = false, $log_path = __DIR__ . "/../storage", $log_file_name = 'track_log.log')
         {
             $this->_start_time = time();
             $this->_msg = $msg;
-            echo "********************************\n";
-            echo "* {$this->_msg} at {$this->returnTime($this->_start_time)}\n";
-            echo "********************************\n";
+            $this->_log_mode = $log_mode;
+            $this->_log_path = $log_path;
+            $this->_log_file_name = $log_file_name;
+            if ($log_mode) {
+                if (!file_exists($this->_log_path)) {
+                    mkdir($this->_log_path, 0777, true);
+                }
+                $this->_f = fopen($this->_log_path . "/" . $this->_log_file_name, "a+");
+            }
+            $this->printResult("********************************\n");
+            $this->printResult("* {$this->_msg} at {$this->returnTime($this->_start_time)}\n");
+            $this->printResult("********************************\n");
             $this->_current_time = $this->_start_time;
             $this->dataArrayInsert($msg, $this->_start_time);
         }
@@ -44,20 +57,20 @@
         public function addCheckPoint($msg = 'Runs')
         {
             $this->dataArrayInsert($msg, time());
-            echo $msg . ": " . $this->convertResponseTime(time() - $this->_current_time)."\n";
+            $this->printResult($msg . ": " . $this->convertResponseTime(time() - $this->_current_time)."\n");
             $this->_current_time = time();
         }
 
         public function finish()
         {
             $this->_total_second = time() - $this->_start_time;
-            echo "********************************\n";
-            echo "* {$this->_msg} End at {$this->returnTime(time())}\n";
-            echo "*\n*\n";
+            $this->printResult("********************************\n");
+            $this->printResult("* {$this->_msg} End at {$this->returnTime(time())}\n");
             for ($i = 1; $i < count($this->_data); $i++) {
-                echo "* {$this->_data[$i]['name']} : {$this->countPercentage($this->_data[$i]['time'] - $this->_data[$i - 1]['time'])} %\n";
+                $this->printResult("* {$this->_data[$i]['name']} : {$this->countPercentage($this->_data[$i]['time'] - $this->_data[$i - 1]['time'])} %\n");
             }
-            echo "********************************\n";
+            $this->printResult("********************************\n");
+            fclose($this->_f);
         }
 
         private function countPercentage($take_time)
@@ -92,5 +105,14 @@
             $hour = floor($minute / 60);
 
             return "{$hour} hours {$minute} minutes {$second} seconds";
+        }
+
+        private function printResult($string)
+        {
+            if ($this->_log_mode) {
+                fputs($this->_f, $string);
+            } else {
+                echo $string;
+            }
         }
     }
